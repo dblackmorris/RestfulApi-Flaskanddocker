dab4e61b016ddd0473180b968d0a182407cfe1cf
root <root@ip-172-31-84-0.ec2.internal>
2019-08-13 09:41:56 +0000

    Last Commit

---
+++ web/app.py
@@ -0,0 +1,137 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+from pymongo import MongoClient
+import bcrypt
+import boto3
+
+app = Flask(__name__)
+api = Api(app)
+
+client = MongoClient("mongodb://db:27017")
+db = client.SentencesDatabase
+users = db["Users"]
+
+class Register(Resource):
+    def post(self):
+        #Step 1 is to get posted data by the user
+        postedData = request.get_json()
+
+        #Get the data
+        username = postedData["username"]
+        password = postedData["password"] #"123xyz"
+
+
+        hashed_pw = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+
+        #Store username and pw into the database
+        users.insert({
+            "Username": username,
+            "Password": hashed_pw,
+            "EC2-Details": "",
+            "Tokens":3
+        })
+
+        retJson = {
+            "status": 200,
+            "msg": "You successfully signed up for the API"
+        }
+        return jsonify(retJson)
+
+def verifyPw(username, password):
+    hashed_pw = users.find({
+        "Username":username
+    })[0]["Password"]
+
+    if bcrypt.hashpw(password.encode('utf8'), hashed_pw) == hashed_pw:
+        return True
+    else:
+        return False
+
+def countTokens(username):
+    tokens = users.find({
+        "Username":username
+    })[0]["Tokens"]
+    return tokens
+
+class Store(Resource):
+    def post(self):
+        
+        postedData = request.get_json()
+
+        
+        username = postedData["username"]
+        password = postedData["password"]
+        ec2 = postedData["ec2"]
+
+        
+        correct_pw = verifyPw(username, password)
+
+        if not correct_pw:
+            retJson = {
+                "status":302
+            }
+            return jsonify(retJson)
+       
+        num_tokens = countTokens(username)
+        if num_tokens <= 0:
+            retJson = {
+                "status": 301
+            }
+            return jsonify(retJson)
+
+        s3_client = boto3.client('s3')
+        s3_client.create_bucket(Bucket=ec2)
+        users.update({
+            "Username":username
+        }, {
+            "$set":{
+                "EC2-Details":ec2,
+                "Tokens":num_tokens-1
+                }
+        })
+
+        retJson = {
+            "status":200,
+            "msg":"Sentence saved successfully"
+        }
+        return jsonify(retJson)
+
+class Get(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        password = postedData["password"]
+
+        #Step 3 verify the username pw match
+        correct_pw = verifyPw(username, password)
+        if not correct_pw:
+            retJson = {
+                "status":302
+            }
+            return jsonify(retJson)
+
+
+
+
+        sentence = users.find({
+            "Username": username
+        })[0]["EC2-Details"]
+        retJson = {
+            "status":200,
+            "sentence": str(sentence)
+        }
+
+        return jsonify(retJson)
+
+
+
+
+api.add_resource(Register, '/register')
+api.add_resource(Store, '/store')
+api.add_resource(Get, '/get')
+
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0')
+
