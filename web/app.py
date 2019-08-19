6740ab1afb1419c5874151d33ec313822ce87c87
Ritik Dubey <ritik.dy@gmail.com>
2019-08-19 08:05:28 +0000

    Updating code to store data

--- web/app.py
+++ web/app.py
@@ -23,6 +23,8 @@ def checkPostedData(postedData,functionName):
             return 302
         else :
             return 200
+    else :
+        return 404
 class Register(Resource):
     def post(self):
         
@@ -75,44 +77,59 @@ class Store(Resource):
     def post(self):
         
         postedData = request.get_json()
+        sCode=checkPostedData(postedData,"Store")
+        if sCode==301:
+            retJson={
+                    "status":sCode,
+                    "msg": "Please enter username,password and bucket"
+                    }
+            return jsonify(retJson)
+        elif sCode==302:
+            retJson={
+                    "status":sCode,
+                    "message": "empty sting as in bucket name is not supported"
+                    }
+            return jsonify(retJson)
+        else :
+            
+        
+            username = postedData["username"]
+            password = postedData["password"]
+            ec2 = postedData["ec2"]
 
         
-        username = postedData["username"]
-        password = postedData["password"]
-        ec2 = postedData["ec2"]
+            correct_pw = verifyPw(username, password)
 
-        
-        correct_pw = verifyPw(username, password)
-
-        if not correct_pw:
-            retJson = {
-                "status":302
-            }
-            return jsonify(retJson)
-       
-        num_tokens = countTokens(username)
-        if num_tokens <= 0:
-            retJson = {
-                "status": 301
-            }
-            return jsonify(retJson)
-
-        s3_client = boto3.client('s3')
-        s3_client.create_bucket(Bucket=ec2)
-        users.update({
-            "Username":username
-        }, {
-            "$set":{
-                "EC2-Details":ec2,
-                "Tokens":num_tokens-1
+            if not correct_pw:
+                setJson = {
+                    "status":502
                 }
-        })
+                return jsonify(setJson)
+       
+            num_tokens = countTokens(username)
+            if num_tokens <= 0:
+                qetJson = {
+                    "status": 301
+                    }
+                return jsonify(qetJson)
 
-        retJson = {
-            "status":200,
-            "msg":"Sentence saved successfully"
-        }
-        return jsonify(retJson)
+            s3_client = boto3.client('s3')
+            s3_client.create_bucket(Bucket=ec2)
+            
+            users.update({
+                "Username":username
+            }, {
+                "$set":{
+                    "EC2-Details":ec2,
+                    "Tokens":num_tokens-1
+                    }
+            })
+
+            retJson = {
+                "status":sCode,
+                "msg":"Data saved successfully"
+            }
+            return jsonify(retJson)
 
 class Get(Resource):
     def post(self):
